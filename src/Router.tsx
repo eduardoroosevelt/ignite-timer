9071a1c343e39e0b1cf58bfe4a64f61d07f6b0ef
eduardoroosevelt <eduardo.roosevelt.de.oliveira@gmail.com>
2022-08-01 21:25:05 -0400

    Estilizando a pagina home

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
-import { DefaultLayout } from './layouts/DefaultLayout'
+import { DefaultLayout } from './layouts/DefaultLayout/Index'
+
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 
